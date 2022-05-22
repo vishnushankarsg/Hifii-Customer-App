5b1d620340d2f839b4b8c5056f6f21a7d6ae6ac3
orbitain <orbitain@pm.me>
2022-05-22 19:31:22 +0530

    Initial

---
+++ android/app/src/main/kotlin/com/hifii/customer/customer/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.hifii.customer.customer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const CustomerApp());
+}
+
+class CustomerApp extends StatelessWidget {
+  const CustomerApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
